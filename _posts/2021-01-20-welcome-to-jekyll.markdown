c45088731b3a83fce7cf63fcc9f05243194119b1
Alexandros Mylonas <alex.myl2000@gmail.com>
2022-06-23 04:47:39 +0300

    Add files via upload

---
+++ _posts/2021-01-20-welcome-to-jekyll.markdown
@@ -0,0 +1,11 @@
+---
+layout: post
+title:  "Finally, my first post"
+date:   2022-06-23
+categories: general
+---
+After so many, non-continuous, tries, I finally completed the creation of my jekyll blog!
+
+I ditched Radioastronomy exam, so I had some time to create this!
+
+Let's see how that will go
---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Finally, my first post",
+        "excerpt":"After so many, non-continuous, tries, I finally completed the creation of my jekyll blog! I ditched Radioastronomy exam, so I had some time to create this! Let’s see how that will go ","categories": ["general"],
+        "tags": [],
+        "url": "/general/2022/06/23/welcome-to-jekyll.html"
+      }]
